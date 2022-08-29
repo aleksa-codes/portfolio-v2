e54c9aaeceb6f27a8afbb040f2898e03b9d40b3f
aleksastojsic <stojsic.aleksa@gmail.com>
2022-08-30 00:30:17 +0200

    Fix: add sitemap fix 2

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: process.env.SITE_URL || 'https://aleksa.vercel.app',
-  generateRobotsTxt: true // (optional)
+  generateRobotsTxt: true, // (optional)
+  generateIndexSitemap: false // (optional)
   // ...other options
 };
